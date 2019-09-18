1c5bd91a834e01af9f74ac395a5cf21aa05edc6c
Matthías Ragnarsson <mattir@protonmail.com>
2019-09-18 13:14:29 -0500

    Fixing altgr issue. I just need to reapply xkb layout on keyboard plugin to have it work.

--- keyboards/planck/keymaps/straumer/keymap.c
+++ keyboards/planck/keymaps/straumer/keymap.c
@@ -48,7 +48,6 @@ enum planck_keycodes {
 #define SECOND MO(_SECOND)
 #define THIRD MO(_THIRD)
 #define FOURTH MO(_FOURTH)
-#define ALTGR RALT(KC_RGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -67,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   ALTGR,    SECOND,  THIRD,   FOURTH,  KC_RGHT
+    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_ALGR, SECOND,  THIRD,   FOURTH,  KC_RGHT
 ),
 
 /* Colemak
