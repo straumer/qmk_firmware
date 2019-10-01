e9274cbea932dfaf2d3358410c3109098eb71226
Matthías Ragnarsson <mattir@protonmail.com>
2019-10-01 08:44:01 -0500

    Added right control and alt.

--- keyboards/planck/keymaps/straumer/keymap.c
+++ keyboards/planck/keymaps/straumer/keymap.c
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Space |Lower |Raise |AltGr |Moves |Funcs | Left |Right |
+ * | Brite| Ctrl | Alt  | GUI  |Space |Lower |Raise |AltGr |Moves |Funcs | Ctrl | Alt  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_ALGR, MOVES,   FUNCS,   KC_LEFT, KC_RGHT
+    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_ALGR, MOVES,   FUNCS,   KC_RCTL, KC_RALT
 ),
 
 /* Colemak
